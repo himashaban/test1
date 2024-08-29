2d5f0402c4a46afe3d613e66b12519e958427606
himashaban <himashaban75@gmail.com>
2024-08-29 20:31:49 +0300

    first commit

---
+++ template/templateName.jsx
@@ -0,0 +1,16 @@
+import React,{useState,useEffect} from 'react'
+// import style from '../template/templateName.module.css'
+
+
+export default function templateName() {
+    useEffect(() => {}, [])
+
+    
+const [first, setfirst] = useState(second)
+  return <>
+  <h2 className={style.s}>templateName</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus beatae dolores sunt adipisci voluptate.</p>
+  
+  
+  </>
+}
